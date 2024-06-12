897ac9e3140c90379fbc5c527a9d244cbc8753f2
Mantas-eng <68243914+Mantas-eng@users.noreply.github.com>
2024-06-12 16:40:59 +0300

    Ready To Deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     webpack: (config, { isServer }) => {
-      // Ignore styles and app directories
       if (!isServer) {
         config.resolve.alias['styles'] = false;
         config.resolve.alias['app'] = false;
