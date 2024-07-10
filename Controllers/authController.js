62f6f9966997057129c99361b7d40230a0fd925e
Mantas-eng <68243914+Mantas-eng@users.noreply.github.com>
2024-07-10 10:30:40 +0300

    Added auth for  Admin users

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -24,7 +24,7 @@ const authController = {
       }
 
       const token = jwt.sign(
-        { userId: user._id, role: user.role },
+        { userId: user._id, email: user.email, role: user.role,},
         process.env.JWT_SECRET,
         { expiresIn: '1h' }
       );
