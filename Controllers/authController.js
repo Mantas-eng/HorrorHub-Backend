cb79e54e78219e7f0dca40813e98c8f251c19682
Mantas-eng <68243914+Mantas-eng@users.noreply.github.com>
2024-07-10 16:29:43 +0300

    Ready to deploy

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,144 +1,155 @@
-  const nodemailer = require('nodemailer');
-  const jwt = require('jsonwebtoken');
-  const bcrypt = require('bcryptjs');
-  const User = require('../models/User');
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,  
-      pass: process.env.EMAIL_PASS  
-    }
-  });
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
-  const authController = {
-    login: async (req, res) => {
-      const { email, password } = req.body;
+const authController = {
+  login: async (req, res) => {
+    const { email, password } = req.body;
 
-      try {
-        const user = await User.findOne({ email });
-        if (!user) {
-          return res.status(404).json({
-            message: 'User with such email not found'
-          });
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-          return res.status(401).json({
-            message: 'Incorrect password'
-          });
-        }
-
-        const token = jwt.sign(
-          { userId: user._id, role: user.role },
-          process.env.JWT_SECRET,
-          { expiresIn: '1h' }
-        );
-
-        res.status(200).json({
-          message: 'Login successful',
-          token,
-          user: { id: user._id, username: user.username, email: user.email, role: user.role }
-        });
-      } catch (error) {
-        res.status(500).json({
-          message: error.message
-        });
-      }
-    },
-
-    register: async (req, res) => {
-      const { username, email, password } = req.body;
-
-      try {
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-          return res.status(400).json({
-            message: 'User with this email already exists'
-          });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const verificationToken = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '1d' });
-
-        const newUser = new User({
-          username,
-          email,
-          password: hashedPassword,
-          role: 'user',
-          verificationToken
-        });
-        await newUser.save();
-
-        const emailSent = await sendVerificationEmail(email, verificationToken);
-
-        if (!emailSent) {
-          throw new Error('Failed to send verification email');
-        }
-
-        res.status(201).json({
-          message: 'Registration successful. Verification email sent.',
-          user: { id: newUser._id, username: newUser.username, email: newUser.email, role: newUser.role }
-        });
-      } catch (error) {
-        res.status(500).json({
-          message: error.message
-        });
-      }
-    },
-
-    verifyEmail: async (req, res) => {
-      const { token } = req.query;
-    
-      try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findOne({ email: decoded.email });
-    
-        if (!user) {
-          return res.status(400).json({
-            message: 'Invalid token or user not found'
-          });
-        }
-    
-        if (user.verified) {
-          return res.status(400).json({
-            message: 'Email is already verified'
-          });
-        }
-    
-        user.verified = true;
-        await user.save();
-    
-        res.redirect('/login'); 
-      } catch (error) {
-        res.status(500).json({
-          message: 'Email verification failed'
-        });
-      }
-    }
-  };
-
-  const sendVerificationEmail = async (email, verificationToken) => {
     try {
-      const mailOptions = {
-        from: process.env.EMAIL_USER,  
-        to: email,             
-        subject: 'Patvirtinkite savo el. paštą',
-        html: `
-          <p>Prašome paspausti šią nuorodą norėdami patvirtinti savo el. paštą:</p>
-          <p><a href="http://horrorhub-backend.onrender.com/verify-email?token=${verificationToken}">Patvirtinti el. paštą</a></p>
-        `
-      };
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({
+          message: 'User with such email not found'
+        });
+      }
 
-      const info = await transporter.sendMail(mailOptions);
-      console.log('El. laiškas išsiųstas: ', info.response);
-      return true;
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({
+          message: 'Incorrect password'
+        });
+      }
+
+      const token = jwt.sign(
+        { userId: user._id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+      );
+
+      res.status(200).json({
+        message: 'Login successful',
+        token,
+        user: { id: user._id, username: user.username, email: user.email, role: user.role }
+      });
     } catch (error) {
-      console.error('Nepavyko išsiųsti el. laiško: ', error);
-      return false;
+      res.status(500).json({
+        message: error.message
+      });
     }
-  };
+  },
 
-  module.exports = authController;
+  register: async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          message: 'User with this email already exists'
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const verificationToken = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+      const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+        role: 'user',
+        verificationToken
+      });
+      await newUser.save();
+
+      const emailSent = await sendVerificationEmail(email, verificationToken);
+
+      if (!emailSent) {
+        throw new Error('Failed to send verification email');
+      }
+
+      res.status(201).json({
+        message: 'Registration successful. Verification email sent.',
+        user: { id: newUser._id, username: newUser.username, email: newUser.email, role: newUser.role }
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message
+      });
+    }
+  },
+
+  verifyEmail: async (req, res) => {
+    const { token } = req.query;
+
+    try {
+      if (!token) {
+        return res.status(400).json({
+          message: 'Token must be provided'
+        });
+      }
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findOne({ email: decoded.email });
+
+      if (!user) {
+        return res.status(400).json({
+          message: 'Invalid token or user not found'
+        });
+      }
+
+      if (user.verified) {
+        return res.status(400).json({
+          message: 'Email is already verified'
+        });
+      }
+
+      user.verified = true;
+      await user.save();
+
+      // Instead of redirecting, send JSON response indicating success
+      res.status(200).json({
+        message: 'Email verified successfully'
+      });
+
+    } catch (error) {
+      console.error('Email verification failed:', error);
+      res.status(500).json({
+        message: 'Email verification failed'
+      });
+    }
+  }
+};
+
+const sendVerificationEmail = async (email, verificationToken) => {
+  try {
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Patvirtinkite savo el. paštą',
+      html: `
+        <p>Prašome paspausti šią nuorodą norėdami patvirtinti savo el. paštą:</p>
+        <p><a href="http://horrorhub-backend-3.onrender.com/verify-email?token=${verificationToken}">Patvirtinti el. paštą</a></p>
+      `
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log('El. laiškas išsiųstas: ', info.response);
+    return true;
+  } catch (error) {
+    console.error('Nepavyko išsiųsti el. laiško: ', error);
+    return false;
+  }
+};
+
+module.exports = authController;
